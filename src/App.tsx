2781feb22175806d1b3fc87682770ef5b831e11e
justerest <justerest@gmailcom>
2020-03-04 12:37:35 +0500

    feat: init todo list view

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
 
+import { TodoList } from './core/TodoList';
+
 export class App extends React.Component {
+  todoList: TodoList = this.createTodoList();
+
   render(): any {
-    return <h1>Hello!</h1>;
+    return (
+      <React.Fragment>
+        <header>
+          <h1>Todo List App</h1>
+        </header>
+        <main>
+          <TodoListCmp todoList={this.todoList}></TodoListCmp>
+          <AddTodoCmp todoList={this.todoList}></AddTodoCmp>
+        </main>
+      </React.Fragment>
+    );
+  }
+
+  componentDidMount(): void {
+    setInterval(() => this.forceUpdate(), 500);
+  }
+
+  private createTodoList(): TodoList {
+    const todoList = new TodoList();
+    todoList.add('Initial created Todo');
+    return todoList;
   }
 }
+
+export const TodoListCmp: React.FC<{ todoList: TodoList }> = ({ todoList }) => {
+  return (
+    <div>
+      <h2>What to do?</h2>
+      <ul>
+        {todoList.getItems().map((todo) => (
+          <li key={todo.getTitle()}>{todo.getTitle()}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export const AddTodoCmp: React.FC<{ todoList: TodoList }> = ({ todoList }) => {
+  return <button onClick={() => todoList.add(`Todo ${todoList.getItems().length}`)}>Add</button>;
+};
